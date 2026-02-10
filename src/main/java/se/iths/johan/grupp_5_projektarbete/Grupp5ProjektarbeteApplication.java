863cc8632a02592fe6e38dcf9397369265f361c8
johan <johan.persson@iths.se>
2026-02-10 11:07:42 +0100

    Initial commit

---
+++ src/main/java/se/iths/johan/grupp_5_projektarbete/Grupp5ProjektarbeteApplication.java
@@ -0,0 +1,13 @@
+package se.iths.johan.grupp_5_projektarbete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupp5ProjektarbeteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupp5ProjektarbeteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/johan/grupp_5_projektarbete/Grupp5ProjektarbeteApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.johan.grupp_5_projektarbete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupp5ProjektarbeteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
