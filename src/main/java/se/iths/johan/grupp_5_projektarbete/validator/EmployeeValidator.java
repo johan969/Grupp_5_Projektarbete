891c3c168738cbba6f830f95355d7b8b430caa61
AMT94-star <aslihan.taskin94@gmail.com>
2026-02-11 21:03:38 +0100

    uppdaterat metoderna, tog bort employee objekt som värdeparameter.

--- src/main/java/se/iths/johan/grupp_5_projektarbete/validator/EmployeeValidator.java
+++ src/main/java/se/iths/johan/grupp_5_projektarbete/validator/EmployeeValidator.java
@@ -2,7 +2,6 @@ package se.iths.johan.grupp_5_projektarbete.validator;
 
 import org.springframework.stereotype.Component;
 import se.iths.johan.grupp_5_projektarbete.exception.EmployeeValidationException;
-import se.iths.johan.grupp_5_projektarbete.model.Employee;
 
 import java.util.List;
 
@@ -15,34 +14,36 @@ public class EmployeeValidator {
             "Employee", "Consult", "Leased"
     );
 
-    public void validateEmployee(Employee employee) {
-        if (employee.getName() == null || employee.getName().isBlank()) {
+    public void validateEmployee(String name) {
+        if (name == null || name.isBlank()) {
             throw new EmployeeValidationException("Employee name is empty");
         }
     }
 
-    public void validateSalary(Employee employee) {
-        if (employee.getMonthlySalary() <= 0) {
+    public void validateSalary(int salary) {
+        int min = 0, max = 200000;
+        if (salary < min || salary > max) {
             throw new EmployeeValidationException("Employee monthly salary is invalid");
         }
     }
 
     //validate för anställningsform
-    public void validateEmploymentType(Employee employee) {
-        if (!employmentType.contains(employee.getEmploymentType())) {
+    public void validateEmploymentType(String employment) {
+        if (!employmentType.contains(employment)) {
             throw new EmployeeValidationException("There is no such employment type");
         }
     }
 
-    public void validateEmploymentPercentage(Employee employee) {
-        if (employee.getEmploymentPercentage() < 0 || employee.getEmploymentPercentage() > 100) {
+    public void validateEmploymentPercentage(int percentage) {
+        int min = 0, max = 100;
+        if (percentage < min || percentage > max) {
             throw new EmployeeValidationException("Employment percentage must be between 0 and 100");
         }
     }
 
     //validering för anställnings titel
-    public void validateEmployeeType(Employee employee) {
-        if (!employeeType.contains(employee.getEmployeeType())) {
+    public void validateEmployeeType(String typeOfEmployee) {
+        if (!employeeType.contains(typeOfEmployee)) {
             throw new EmployeeValidationException("There is no such employee type");
         }
     }
