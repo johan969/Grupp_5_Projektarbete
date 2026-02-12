60b5162888d9d0e9ac598fcafeca2622ad84e62e
AMT94-star <aslihan.taskin94@gmail.com>
2026-02-12 14:41:34 +0100

    init commit av employee service enligt CRUD metodik. Uppdaterade även exception filen, värde param ändrades från string till long

--- src/main/java/se/iths/johan/grupp_5_projektarbete/exception/EmployeeNotFoundException.java
+++ src/main/java/se/iths/johan/grupp_5_projektarbete/exception/EmployeeNotFoundException.java
@@ -1,7 +1,7 @@
 package se.iths.johan.grupp_5_projektarbete.exception;
 
 public class EmployeeNotFoundException extends RuntimeException {
-    public EmployeeNotFoundException(String message) {
-        super(message);
+    public EmployeeNotFoundException(Long id) {
+        super("Employee with id: " + id + " not found");
     }
 }
---
+++ src/main/java/se/iths/johan/grupp_5_projektarbete/service/EmployeeService.java
@@ -0,0 +1,70 @@
+package se.iths.johan.grupp_5_projektarbete.service;
+
+import org.springframework.stereotype.Service;
+import se.iths.johan.grupp_5_projektarbete.exception.EmployeeNotFoundException;
+import se.iths.johan.grupp_5_projektarbete.model.Employee;
+import se.iths.johan.grupp_5_projektarbete.repository.EmployeeRepository;
+import se.iths.johan.grupp_5_projektarbete.validator.EmployeeValidator;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+    private final EmployeeValidator employeeValidator;
+
+    public EmployeeService(EmployeeRepository repository, EmployeeValidator validator) {
+        this.employeeRepository = repository;
+        this.employeeValidator = validator;
+    }
+
+    //get all
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    //get one
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).
+                orElseThrow(() -> new EmployeeNotFoundException(id));
+    }
+
+    //create, validerar så att entiteten inte bryter mot några regler innan create
+    public Employee createEmployee(Employee employee) {
+        employeeValidator.validateEmployee(employee.getName());
+        employeeValidator.validateSalary(employee.getMonthlySalary());
+        employeeValidator.validateEmploymentType(employee.getEmploymentType());
+        employeeValidator.validateEmploymentPercentage(employee.getEmploymentPercentage());
+        employeeValidator.validateEmployeeType(employee.getEmployeeType());
+        return employeeRepository.save(employee);
+    }
+
+    //update
+    public Employee updateEmployee(Long id, Employee updatedEmployee) {
+        Employee current = employeeRepository.findById(id).
+                orElseThrow(() -> new EmployeeNotFoundException(id));
+        //validering av update innan infon lagras över till current
+        employeeValidator.validateEmployee(updatedEmployee.getName());
+        employeeValidator.validateSalary(updatedEmployee.getMonthlySalary());
+        employeeValidator.validateEmploymentType(updatedEmployee.getEmploymentType());
+        employeeValidator.validateEmploymentPercentage(updatedEmployee.getEmploymentPercentage());
+        employeeValidator.validateEmployeeType(updatedEmployee.getEmployeeType());
+
+        //uppdaterar existing med den validerade infon från update employee
+        current.setName(updatedEmployee.getName());
+        current.setMonthlySalary(updatedEmployee.getMonthlySalary());
+        current.setEmploymentType(updatedEmployee.getEmploymentType());
+        current.setEmploymentPercentage(updatedEmployee.getEmploymentPercentage());
+        current.setEmployeeType(updatedEmployee.getEmployeeType());
+
+        return employeeRepository.save(current);
+    }
+
+    //delete, letar efter employee med värdeparam, hittas ej så kastas annars delete
+    public void deleteEmployee(Long id) {
+        Employee current = employeeRepository.findById(id).
+                orElseThrow(() -> new EmployeeNotFoundException(id));
+        employeeRepository.delete(current);
+    }
+}
