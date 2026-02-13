80b58fae39527394c77b5a4f8e8f0c6f603daa44
SaraSoltani98 <SaraSoltani.SaraSoltani@iths.se>
2026-02-13 15:05:43 +0100

    Remove validator files from this PR

--- src/main/java/se/iths/johan/grupp_5_projektarbete/exception/LeaveValidationException.java
+++
@@ -1,7 +0,0 @@
-package se.iths.johan.grupp_5_projektarbete.exception;
-
-public class LeaveValidationException extends RuntimeException {
-    public LeaveValidationException(String message) {
-        super(message);
-    }
-}
