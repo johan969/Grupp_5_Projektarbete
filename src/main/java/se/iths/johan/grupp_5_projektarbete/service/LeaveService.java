3be8fe5742bc7c5b86a9a0a9b038ad70c20448e6
SaraSoltani98 <SaraSoltani.SaraSoltani@iths.se>
2026-02-17 12:06:21 +0100

    Add LeaveValidator usage in LeaveService

--- src/main/java/se/iths/johan/grupp_5_projektarbete/service/LeaveService.java
+++ src/main/java/se/iths/johan/grupp_5_projektarbete/service/LeaveService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import se.iths.johan.grupp_5_projektarbete.exception.LeaveNotFoundException;
 import se.iths.johan.grupp_5_projektarbete.model.Leave;
 import se.iths.johan.grupp_5_projektarbete.repository.LeaveRepository;
+import se.iths.johan.grupp_5_projektarbete.validator.LeaveValidator;
 
 import java.util.List;
 
@@ -11,31 +12,32 @@ import java.util.List;
 public class LeaveService {
 
     private final LeaveRepository leaveRepository;
+    private final LeaveValidator leaveValidator;
 
-    public LeaveService(LeaveRepository leaveRepository) {
+    public LeaveService(LeaveRepository leaveRepository, LeaveValidator leaveValidator) {
         this.leaveRepository = leaveRepository;
+        this.leaveValidator = leaveValidator;
     }
 
-    // GET (alla)
     public List<Leave> getAll() {
         return leaveRepository.findAll();
     }
 
-    // GET (en)
     public Leave getById(Long id) {
         return leaveRepository.findById(id)
                 .orElseThrow(() -> new LeaveNotFoundException("Leave with id " + id + " was not found"));
     }
 
-    // POST (skapa)
     public Leave create(Leave leave) {
+        validateLeave(leave);
         return leaveRepository.save(leave);
     }
 
-    // PUT (uppdatera)
     public Leave update(Long id, Leave updated) {
         Leave existing = leaveRepository.findById(id)
                 .orElseThrow(() -> new LeaveNotFoundException("Leave with id " + id + " was not found"));
+
+        validateLeave(updated);
 
         existing.setEmployeeName(updated.getEmployeeName());
         existing.setStartDate(updated.getStartDate());
@@ -45,11 +47,16 @@ public class LeaveService {
         return leaveRepository.save(existing);
     }
 
-    // DELETE (ta bort)
     public void delete(Long id) {
         Leave existing = leaveRepository.findById(id)
                 .orElseThrow(() -> new LeaveNotFoundException("Leave with id " + id + " was not found"));
 
         leaveRepository.delete(existing);
+    }
+
+    private void validateLeave(Leave leave) {
+        leaveValidator.validateEmployeeName(leave.getEmployeeName());
+        leaveValidator.validateStartDate(leave.getStartDate());
+        leaveValidator.validateEndDate(leave.getStartDate(), leave.getEndDate());
     }
 }
